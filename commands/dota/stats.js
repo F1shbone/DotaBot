322ae0acdb24beca435d73735fa8d4374b6c3230
F1shbone <DJ_Side@t-online.de>
2017-10-07 03:35:16 +0200

    Changed Stats to use API

--- commands/dota/stats.js
+++ commands/dota/stats.js
@@ -5,9 +5,11 @@ const path = require('path')
  *
  */
 
+const Discord = require('discord.js')
 const Commando = require('discord.js-commando')
 const SQLite3 = require('sqlite3').verbose()
 const Axios = require('axios')
+const API = require('./api')
 const Logger = require('../../logger')
 
 class StatsCommand extends Commando.Command {
@@ -21,6 +23,7 @@ class StatsCommand extends Commando.Command {
     })
 
     this.DB = new SQLite3.Database(path.join(__dirname, '..', 'db.sqlite'))
+    this.LIMIT = 20
   }
 
   async run (message, args) {
@@ -34,45 +37,76 @@ class StatsCommand extends Commando.Command {
       }, async (err, row) => {
         if (!err && row) {
           try {
-            let stats = await Axios.get(`https://api.opendota.com/api/players/${row.dota_id}`)
-            let winlose = await Axios.get(`https://api.opendota.com/api/players/${row.dota_id}/wl`)
-            let mmr = {
-              solo: stats.data.solo_competitive_rank,
-              party: stats.data.competitive_rank,
-              estimate: stats.data.mmr_estimate.estimate
-            }
-            let winrate = winlose.data.win * 100 / (winlose.data.win + winlose.data.lose)
-
-            message.channel.send({
-              embed: {
-                author: {
-                  name: stats.data.profile.personaname,
-                  icon_url: stats.data.profile.avatar
-                },
-                timestamp: new Date(),
-                fields: [
-                  {
-                    name: 'Solo MMR',
-                    value: `\`${mmr.solo || 'uncalibrated'}\``,
-                    inline: true
-                  },
-                  {
-                    name: 'Party MMR',
-                    value: `\`${mmr.party || 'uncalibrated'}\``,
-                    inline: true
-                  },
-                  {
-                    name: 'Estimate MMR',
-                    value: `\`${mmr.estimate}\``,
-                    inline: true
-                  },
-                  {
-                    name: 'Winrate',
-                    value: `\`${Math.round(winrate * 100) / 100}%\``
-                  }
-                ]
+            let response = await Axios.all([
+              API.Players.Data.queue(row.dota_id),
+              API.Players.RecentMatches.queue(row.dota_id)
+            ])
+            let parser = (data, recentMatches) => {
+              return {
+                data: API.Players.Data.parse(data),
+                recentMatches: API.Players.RecentMatches.parse(recentMatches)
               }
-            })
+            }
+            let data = parser.apply(null, response)
+            let lastHitsProMin = data.recentMatches.Damage.last_hits / data.recentMatches.Stats.duration.getMinutes()
+            let embed = new Discord.RichEmbed()
+              .setAuthor(data.data.Account.name, data.data.Account.avatar.small)
+              .setDescription('_The following are averages and percentages based on the last 20 parsed matches_')
+              .setTimestamp()
+              // General
+              .addField(
+                'General',
+                `Winrate: ${data.recentMatches.Stats.wins}%
+                 KDA: **${data.recentMatches.General.kills}**/**${data.recentMatches.General.deaths}**/**${data.recentMatches.General.assists}**
+                 Game duration: ${data.recentMatches.Stats.duration_formatted().replace('00:', '')}
+                 Ranked: ${data.recentMatches.Stats.ranked}%
+                 Party: ${data.recentMatches.Stats.party}%
+                `.replace(/^(\s)*/gm, ''),
+                true
+              )
+              // MMR
+              .addField(
+                'MMR',
+                `Solo: \`${data.data.MMR.solo}\`
+                  Party: \`${data.data.MMR.party}\`
+                  Estimated: \`${data.data.MMR.estimated}\`
+                `.replace(/^(\s)*/gm, ''),
+                true
+              )
+              // Economy
+              //     Farm from Jungle: ${Math.round((stats.neutral_kills * 100 / stats.last_hits) * 100) / 100}%
+              .addField(
+                'Economy',
+                `GPM: ${data.recentMatches.Economy.gpm}
+                 XPM: ${data.recentMatches.Economy.xpm}
+                 Last Hits/min: ${Math.round(lastHitsProMin * 100) / 100}
+                `.replace(/^(\s)*/gm, ''),
+                true
+              )
+              // Heroes
+              .addField(
+                'Heroes',
+                `<:attr_strength:365158484511293441> ${data.recentMatches.Heroes.strength}%
+                  <:attr_agility:365158657941569536> ${data.recentMatches.Heroes.agility}%
+                  <:attr_intelligence:365158714379993098> ${data.recentMatches.Heroes.intelligence}%
+                `.replace(/^(\s)*/gm, ''),
+                true
+              )
+              // Laning
+              .addField(
+                'Laning',
+                `Safe Lane: ${data.recentMatches.Role.safe}%
+                  Mid Lane: ${data.recentMatches.Role.mid}%
+                  Off Lane: ${data.recentMatches.Role.off}%
+                  Jungle: ${data.recentMatches.Role.jungle}%
+                  Roaming: ${data.recentMatches.Role.roaming}%
+                `.replace(/^(\s)*/gm, ''),
+                true
+              )
+              // Spacer
+              .addBlankField(true)
+
+            message.channel.send({embed})
           } catch (e) {
             console.error(e)
           }
